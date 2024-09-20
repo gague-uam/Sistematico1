febd6bfebfea065aa90617d7ddf0fa16152d61b3
Daniel Gutiérrez <d.xander.gutierrez@gmail.com>
2024-09-20 08:51:33 -0600

    Ejercicio 4

---
+++ controlAcceso/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace controlAcceso
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbUser = new System.Windows.Forms.TextBox();
+            this.tbPassword = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Usuario:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Contraseña:";
+            // 
+            // tbUser
+            // 
+            this.tbUser.Location = new System.Drawing.Point(85, 39);
+            this.tbUser.Name = "tbUser";
+            this.tbUser.Size = new System.Drawing.Size(100, 22);
+            this.tbUser.TabIndex = 2;
+            this.tbUser.TextChanged += new System.EventHandler(this.tbUser_TextChanged);
+            // 
+            // tbPassword
+            // 
+            this.tbPassword.Location = new System.Drawing.Point(110, 70);
+            this.tbPassword.Name = "tbPassword";
+            this.tbPassword.Size = new System.Drawing.Size(100, 22);
+            this.tbPassword.TabIndex = 3;
+            this.tbPassword.TextChanged += new System.EventHandler(this.tbPassword_TextChanged);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(267, 37);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 4;
+            this.btnAdd.Text = "Agregar";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.tbPassword);
+            this.Controls.Add(this.tbUser);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbUser;
+        private System.Windows.Forms.TextBox tbPassword;
+        private System.Windows.Forms.Button btnAdd;
+    }
+}
+
---
+++ controlAcceso/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace controlAcceso
+{
+    public partial class Form1 : Form
+    {
+        private int attempts = 0;
+        private const int maxAttempts = 3;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            int user;
+            int password;
+
+            // Verificar si el usuario ha introducido valores numéricos
+            bool isUserValid = int.TryParse(tbUser.Text, out user);
+            bool isPasswordValid = int.TryParse(tbPassword.Text, out password);
+
+            // Verificar si el nombre de usuario y la contraseña son números enteros
+            if (!isUserValid || !isPasswordValid)
+            {
+                MessageBox.Show("El nombre de usuario y la contraseña deben ser números enteros.", "Error de formato", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            // Verificar las credenciales
+            if (user == 12 && password == 1234)
+            {
+                MessageBox.Show("Inicio de sesión exitoso.", "Acceso concedido", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                // Aquí puedes redirigir al usuario a otra parte de la aplicación si es necesario
+                return;
+            }
+            else
+            {
+                attempts++;
+                if (attempts >= maxAttempts)
+                {
+                    MessageBox.Show("Has excedido el número máximo de intentos. El acceso ha sido bloqueado.", "Acceso denegado", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    // Deshabilitar el botón de inicio de sesión después de 3 intentos fallidos
+                    btnAdd.Enabled = false;
+                }
+                else
+                {
+                    MessageBox.Show($"Credenciales incorrectas. Te quedan {maxAttempts - attempts} intentos.", "Acceso denegado", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void tbUser_TextChanged(object sender, EventArgs e)
+        {
+            // Opcional: Lógica adicional si se desea.
+        }
+
+        private void tbPassword_TextChanged(object sender, EventArgs e)
+        {
+            // Opcional: Lógica adicional si se desea.
+        }
+    }
+}
